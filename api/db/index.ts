a9c7bda5efd673cd9e6876ad4830f81c6f597711
rayat69 <68869946+rayat69@users.noreply.github.com>
2021-02-15 01:37:43 +0600

    Update index.ts

--- api/db/index.ts
+++ api/db/index.ts
@@ -1,4 +1,4 @@
-import { Client } from "https://deno.land/x/mysql/mod";
+import { Client } from "https://deno.land/x/mysql/mod.ts";
 
 export const client = await new Client().connect({
   hostname: "localhost",
